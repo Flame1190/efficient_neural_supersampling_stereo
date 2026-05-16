f9ef6c4cd8f4eec6ec4c29f8cc8db54585302273
Sam Thompson <sam.flame1190@gmail.com>
2026-05-16 20:30:23 +0100

    Started work on displaying output vs input

--- samtest.py
+++ samtest.py
@@ -1,16 +1,19 @@
 import torch
+from torch.nn.functional import layer_norm
 from model.model import ENSS
-
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     model = ENSS(scale_factor=2)
-    color = torch.randn(1, 3, 256, 256)
+    lr_color = torch.randn(1, 3, 256, 256)
     depth = torch.randn(1, 1, 256, 256)
     jitter = torch.randn(1, 2, 1, 1)
     prev_jitter = torch.randn(1, 2, 1, 1)
     motion = torch.randn(1, 2, 256, 256)
     prev_features = torch.randn(1, 1, 512, 512)
     prev_color = torch.randn(1, 3, 512, 512)
-    features, color = model(color, depth, motion, jitter, prev_jitter, prev_features, prev_color)
+    features, hr_color = model(lr_color, depth, motion, jitter, prev_jitter, prev_features, prev_color)
     print(features.shape)
-    print(color.shape)
+    print(hr_color.shape)
+    plt.imshow(hr_color.permute(2, 3, 1))
+    
